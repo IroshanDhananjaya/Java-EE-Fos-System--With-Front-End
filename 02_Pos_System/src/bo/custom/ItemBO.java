cdab977845502a0578b15c27baad3c284aeb7428
Iroshan Dhananjaya <86369828+IroshanDhananjaya@users.noreply.github.com>
2022-05-19 00:08:42 +0530

    init Java EE pos System with Front End

---
+++ 02_Pos_System/src/bo/custom/ItemBO.java
@@ -0,0 +1,21 @@
+package bo.custom;
+
+import bo.SuperBO;
+import dto.ItemDTO;
+
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * @author : Dhananjaya
+ * @since : 0.0.1
+ **/
+public interface ItemBO extends SuperBO {
+    boolean addNewItem(ItemDTO dto , DataSource d)throws SQLException, ClassNotFoundException ;
+    JsonArrayBuilder loadAllItem(DataSource d) throws SQLException, ClassNotFoundException;
+    boolean deleteItem(String id,DataSource dataSource) throws SQLException, ClassNotFoundException;
+    boolean updateItem(ItemDTO i,DataSource dataSource)throws SQLException, ClassNotFoundException;
+    JsonObjectBuilder getItem(String id, DataSource dataSource)throws SQLException, ClassNotFoundException;
+}
---
+++ 02_Pos_System/src/dao/custom/OrderDAO.java
@@ -0,0 +1,11 @@
+package dao.custom;
+
+import dao.CrudDAO;
+import entity.Order;
+
+/**
+ * @author : Dhananjaya
+ * @since : 0.0.1
+ **/
+public interface OrderDAO extends CrudDAO <Order, String> {
+}
